3e04b77ed79788a9ad1b0b2fe0415db2ae672473
Anorld Mukone <manorldsapiens@gmail.com>
2018-11-19 13:03:27 +0300

    user works

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ def main():
     user.signup(user)
 
 
-    login_prompt = input("Would You like to Login?  Enter: 1 for YES, 0 for NO ")
+    login_prompt = raw_input("Would You like to Login?  Enter: 1 for YES, 0 for NO ")
     # text = "jb"
     if login_prompt == 1:
-        username = input("Enter Username: ")
-        password = input("Enter your password: ")
+        username = raw_input("Enter Username: ")
+        password = raw_input("Enter your password: ")
 
         is_logged_in = user.login()
         if is_logged_in:
-            text = str(input("Enter your comment: "))
+            text = str(raw_input("Enter your comment: "))
         else:
             print("You are not logged in: ")
             return
@@ -41,17 +41,12 @@ def main():
         for item in comments:
             if item.id == get_id:
                 item.text = new_comment
-                print(
-                {'id': item.id, 'username': item.username, 'text': item.text, 'timestamp': item.timestamp})
         print("Your comment was updated.")
-
     if edit_ == 3:
         get_id = input("Enter comment Id")
         for item in comments:
             if item.id == get_id:
                 item.remove(get_id)
-                print(
-                    {'id': item.id, 'username': item.username, 'text': item.text, 'timestamp': item.timestamp})1
         print ("Your comment was deleted")
 
 
